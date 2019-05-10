c6677bae80cb843b3cb4f8f2ff81e6440991b6f7
KingOfCramers <32515581+KingOfCramers@users.noreply.github.com>
2019-05-09 20:47:49 -0400

    hashing passwords

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -7,6 +8,18 @@ const userSchema = new Schema({
     },
     password: {
         type: String
+    }
+});
+
+userSchema.pre("save", async function(next){
+    try {
+        // Generate salt, and hash + salt password. Reassign hashed version before save...
+        const salt = await bcrypt.genSalt(10);
+        const passwordHash = await bcrypt.hash(this.password, salt);
+        this.password = passwordHash;
+        next();
+    } catch(err){
+        next(err);
     }
 });
 
--- passport/index.js
+++ passport/index.js
@@ -1,8 +1,11 @@
 const passport = require("passport");
 const JwtStrategy = require("passport-jwt").Strategy;
+const LocalStrategy = require("passport-local").Strategy;
 const { ExtractJwt } = require("passport-jwt");
 const User = require("../models/User");
 
+
+/// JSON WEB TOKENS STRATEGY
 passport.use(
     new JwtStrategy({
         jwtFromRequest: ExtractJwt.fromHeader('authorization'), // Common practice to use authorization...
@@ -21,3 +24,22 @@ passport.use(
         }
     })
 );
+
+// LOCAL STRATEGY
+passport.use(
+    new LocalStrategy({
+        usernameField: 'email', // This defaults to username otherwise, we need to manually set it.
+    }, async (email, password, done) => {
+
+        const user = await User.findOne({ email });
+        // If user doesn't exist, handle this...
+        if(!user){
+            return done(null, false);
+        }
+        // Then check that the password is correct...
+
+        // If not, handle that...
+
+        // Return the user...
+    })
+)
