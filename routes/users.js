2b4b0858d8284740abed5e4a92fb8455aad4a5a7
KingOfCramers <32515581+KingOfCramers@users.noreply.github.com>
2019-05-09 21:42:40 -0400

    refactored routes

--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,17 @@ const { schemas, validateBody } = require("../helpers/routeHelpers")
 
 require("../passport"); /// Must load passport configuration...
 
-router.post("/signup", validateBody(schemas.authSchema), signUp);
-router.post("/signin", validateBody(schemas.authSchema), passport.authenticate("local", { session: false }), signIn);
-router.get("/secret", passport.authenticate('jwt', { session: false }), secret);
+/// Passport middleware strategies
+const passportLocalMiddleware = passport.authenticate("local", { session: false }); // Used for logging in...
+const passportJWTMiddleWare = passport.authenticate('jwt', { session: false }); // Used for getting locked resourcs...
+
+// Simple middleware to validate input from user.
+const inputValidator = validateBody(schemas.authSchema);
+
+router.post("/signup", inputValidator, signUp);
+
+router.post("/signin", inputValidator, passportLocalMiddleware, signIn);
+
+router.get("/secret", passportJWTMiddleWare, secret);
 
 module.exports = router;
